ecd77f9069a12253e66d399a8b6eb99c1028faf0
Gokul Chandrasekaran <gokulchandra93@gmail.com>
2022-07-22 13:08:29 +1000

    loads data from server and uses epoch ms as item ids

--- phase-1/pillars-of-web-programming/index.js
+++ phase-1/pillars-of-web-programming/index.js
@@ -2,7 +2,6 @@
 // * Mark as complete
 // * Add a button that removes all completed tasks
 
-
 const items = [];
 
 function addItem(task) {
@@ -15,15 +14,23 @@ function addItem(task) {
 
     const node = document.createElement('li'); // === <li></li>
 
+    console.log('I Will run before the above console')
+
     node.innerText = task;
 
-    node.id = items.length;
+    node.id = Date.now();
 
     node.addEventListener('click', markAsCompleted);
 
     listNode.appendChild(node);
-    
+
     items.push({ name: task, completed: false });
+}
+
+function onEnter(event) {
+    if (event.keyCode === 13) {
+        getItem();
+    }
 }
 
 function getItem() {
@@ -42,6 +49,32 @@ function markAsCompleted(event) {
     const itemIndex = items.findIndex(item => item.name === event.target.innerText);
     items[itemIndex].completed = true;
 
-    event.target.style['text-decoration'] = 'line-through' 
+    event.target.style['text-decoration'] = 'line-through'
 
 }
+
+/*
+    Async programming.
+    Getting data from the server using `fetch`
+*/
+
+/* 
+    Promise - allows async programming
+    Promise can be in two states: 
+        - Pending 
+        - Fulfilled
+        - Rejected
+
+        When promise changes from Pending -> resolved
+        When promise changes from Pending -> rejected
+*/
+
+
+fetch('https://run.mocky.io/v3/fa1510e4-9ea0-4f4e-a82b-729937f3334e', {
+    mode: 'cors'
+})
+    .then((res) => res.json())
+    .then((items) => items.forEach(item => addItem(item.name)))
+    .catch(err => {
+        console.log(err)
+    })
