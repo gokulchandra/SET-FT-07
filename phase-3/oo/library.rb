1d2792ffa73360e8c634c8360777fcf7c7f050c4
Gokul Chandrasekaran <gokulchandra93@gmail.com>
2022-09-09 14:38:31 +1000

    adds code from domain design class

--- phase-3/oo/library.rb
+++ phase-3/oo/library.rb
@@ -2,21 +2,65 @@
 # Book =>  cover, title, language, available,
 # Section => shelves, name
 # Shelf => capacity, books
+# Member =>
 
 require 'pry'
 
 class Book
   attr_accessor :is_available, :title
-  attr_reader :language
+  attr_reader :language, :rating
   attr_writer :isbn
 
-  def initialize(title, language)
+  def initialize(title, language, rating)
     @title = title
     @language = language
+    @rating = rating
     @is_available = true
   end
 end
 
-book = Book.new 'Ruby 102', 'English'
+class ChildrensBook < Book
+  attr_accessor :is_educational
+  
+  @@genre = 'Children'
+
+  def self.genre
+    @@genre
+  end
+end
+
+class Member
+  attr_accessor :books, :name
+  attr_reader :age, :id
+
+  def initialize(name, age, id)
+    @name = name
+    @age = age
+    @id = id
+    @books = []
+  end
+
+  def borrow(book)
+    binding.pry
+    @books << book if can_borrow? book
+  end
+
+  def can_borrow?(book)
+    return false if @books.count >= 5
+    return @age >= 18 if book.rating == 'R'
+
+    true
+  end
+end
+
+ruby101 = Book.new 'Ruby 101', 'Enligh', 'G'
+ruby102 = Book.new 'Ruby 102', 'English', 'R'
+bob = Member.new 'Bob', 16, 34_234_324
+jim = Member.new 'Jim', 20, 34_234_324
+
+
+kidBook1 = ChildrensBook.new 'Numbers 101', 'Enligh', 'K' 
+kidBook2 = ChildrensBook.new 'Numbers 102', 'Enligh', 'K'
+kidBook3 = ChildrensBook.new 'Numbers 103', 'Enligh', 'K'
 
 binding.pry
